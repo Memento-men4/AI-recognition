4505aa4f80d8a958fef93aa4171712983595af24
sehee-jeong <76215009+sehee-jeong@users.noreply.github.com>
2022-12-02 03:25:58 +0900

    Add files via upload

---
+++ model/train.py
@@ -0,0 +1,360 @@
+import logging
+import os
+import sys
+from typing import NoReturn
+import wandb
+from arguments import DataTrainingArguments, ModelArguments
+from datasets import DatasetDict, load_from_disk, load_metric, load_dataset
+from trainer_qa import QuestionAnsweringTrainer
+from transformers import (
+    AutoConfig,
+    AutoModelForQuestionAnswering,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    EvalPrediction,
+    HfArgumentParser,
+    TrainingArguments,
+    set_seed,
+)
+from utils_qa import check_no_error, postprocess_qa_predictions
+from ray.tune.integration.wandb import wandb_mixin
+
+logger = logging.getLogger(__name__)
+
+@wandb_mixin
+def main():
+    parser = HfArgumentParser(
+        (ModelArguments, DataTrainingArguments, TrainingArguments)
+    )
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    #training_args.per_device_train_batch_size = 4
+    # WandB Init
+    project = "test-project-CG"  
+    entity_name = "level3-nlp-09"
+    display_name =  f"{model_args.model_name_or_path}"
+    wandb.init(project=project, entity=entity_name, name=display_name)
+    # if config in wandb change
+    config = wandb.config
+    if len(dict(config)) != 0:
+        training_args.learning_rate= config["learning_rate"]
+        training_args.num_train_epochs= config["num_train_epochs"]
+        training_args.per_device_train_batch_size= config["per_device_train_batch_size"]
+    else:
+        print("Not Use sweep")
+        pass
+    training_args.report_to = ['wandb']
+    print(f"model is from {model_args.model_name_or_path}")
+    print(f"data is from {data_args.dataset_name}")
+
+    # logging setting
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -    %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    # verbosity & randomSeed
+    logger.info("Training/evaluation parameters %s", training_args)
+    set_seed(training_args.seed)
+
+    datasets = load_dataset('csv', data_files={'train':"../data/train.csv", 
+                                                'validation': '../data/validation.csv'})
+    print(datasets)
+
+    #  Load pretrained model and tokenizer
+    config = AutoConfig.from_pretrained(
+        model_args.config_name
+        if model_args.config_name is not None
+        else model_args.model_name_or_path,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.tokenizer_name
+        if model_args.tokenizer_name is not None
+        else model_args.model_name_or_path,
+        # 'use_fast' = True: rust로 구현된 tokenizer를 사용
+        # False로 설정할 경우 python으로 구현된 tokenizer를 사용할 수 있으며, rust version이 비교적 속도가 빠릅니다.
+        use_fast=True,
+    )
+    model = AutoModelForQuestionAnswering.from_pretrained(
+        model_args.model_name_or_path,
+        from_tf=bool(".ckpt" in model_args.model_name_or_path),
+        config=config,
+    )
+
+    print(
+        type(training_args),
+        type(model_args),
+        type(datasets),
+        type(tokenizer),
+        type(model),
+    )
+
+    if training_args.do_train or training_args.do_eval:
+        run_mrc(data_args, training_args, model_args, datasets, tokenizer, model)
+
+def run_mrc(
+    data_args: DataTrainingArguments,
+    training_args: TrainingArguments,
+    model_args: ModelArguments,
+    datasets: DatasetDict,
+    tokenizer,
+    model,
+) -> NoReturn:
+
+    # Dataset Preprocessing
+    if training_args.do_train:
+        column_names = datasets["train"].column_names
+    else:
+        column_names = datasets["validation"].column_names
+
+    question_column_name = "question" if "question" in column_names else column_names[0]
+    context_column_name = "context" if "context" in column_names else column_names[1]
+    answer_column_name = "answers" if "answers" in column_names else column_names[2]
+
+    # Padding Option: (Question|Context), (Context|Question)
+    pad_on_right = tokenizer.padding_side == "right"
+    # Check Error
+    last_checkpoint, max_seq_length = check_no_error(
+        data_args, training_args, datasets, tokenizer
+    )
+
+    # Train Preprocessing
+    # truncation과 padding을 통해 toknization 진행, stride를 이용해 overflow를 유지하며 각 example들은 이전의 context와 조금씩 겹침
+    def prepare_train_features(examples):
+        tokenized_examples = tokenizer(
+            examples[question_column_name if pad_on_right else context_column_name],
+            examples[context_column_name if pad_on_right else question_column_name],
+            truncation="only_second" if pad_on_right else "only_first",
+            max_length=max_seq_length,
+            stride=data_args.doc_stride,
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            return_token_type_ids=False, # roberta: False / bert:True
+            padding="max_length" if data_args.pad_to_max_length else False,
+        )
+
+        # 길이가 긴 context가 등장할 경우 truncate를 진행해야하므로, 해당 데이터셋을 찾을 수 있도록 mapping 가능한 값이 필요합니다.
+        # token의 캐릭터 단위 position를 찾을 수 있도록 offset mapping을 사용 start_positions과 end_positions을 찾는데 도움이 된다.
+        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+        offset_mapping = tokenized_examples.pop("offset_mapping")
+        tokenized_examples = set_tokenized_examples_start_end_pos(tokenized_examples, sample_mapping, offset_mapping, examples)
+        return tokenized_examples
+
+    def set_tokenized_examples_start_end_pos(tokenized_examples, sample_mapping, offset_mapping, examples):
+        # "start position", "end position" label
+        tokenized_examples["start_positions"] = []
+        tokenized_examples["end_positions"] = []
+
+        for i, offsets in enumerate(offset_mapping):
+            input_ids = tokenized_examples["input_ids"][i]
+            cls_index = input_ids.index(tokenizer.cls_token_id)  # cls index
+
+            # Setting sequence id
+            sequence_ids = tokenized_examples.sequence_ids(i)
+
+            sample_index = sample_mapping[i]
+            answers = eval(examples[answer_column_name][sample_index])
+
+            # if no answer append cls_index
+            if answers["text"] == ' ':
+                tokenized_examples["start_positions"].append(cls_index)
+                tokenized_examples["end_positions"].append(cls_index)
+            else:
+                # Start/end character index
+                start_char = answers["answer_start"]
+                end_char = start_char + len(answers["text"])
+
+                # Start token index
+                token_start_index = 0
+                while sequence_ids[token_start_index] != (1 if pad_on_right else 0):
+                    token_start_index += 1
+
+                # End token index
+                token_end_index = len(input_ids) - 1
+                while sequence_ids[token_end_index] != (1 if pad_on_right else 0):
+                    token_end_index -= 1
+
+                # span check
+                if not (
+                    offsets[token_start_index][0] <= start_char
+                    and offsets[token_end_index][1] >= end_char
+                ):
+                    tokenized_examples["start_positions"].append(cls_index)
+                    tokenized_examples["end_positions"].append(cls_index)
+                else:
+                    # token_start_index 및 token_end_index를 answer의 끝으로 이동합니다. Note) answer가 마지막 단어인 경우 last offset을 따라갈 수 있습니다(edge case). [?]
+                    while (
+                        token_start_index < len(offsets)
+                        and offsets[token_start_index][0] <= start_char
+                    ):
+                        token_start_index += 1
+                    tokenized_examples["start_positions"].append(token_start_index - 1)
+                    while offsets[token_end_index][1] >= end_char:
+                        token_end_index -= 1
+                    tokenized_examples["end_positions"].append(token_end_index + 1)
+        return tokenized_examples
+
+
+    if training_args.do_train:
+        if "train" not in datasets:
+            raise ValueError("--do_train requires a train dataset")
+        train_dataset = datasets["train"]
+
+        # Create train feature
+        train_dataset = train_dataset.map(
+            prepare_train_features,
+            batched=True,
+            num_proc=data_args.preprocessing_num_workers,
+            remove_columns=column_names,
+            load_from_cache_file=not data_args.overwrite_cache,
+        )
+
+    # Validation preprocessing
+    # truncation과 padding을 통해 toknization 진행, stride를 이용해 overflow를 유지하며 각 example들은 이전의 context와 조금씩 겹침
+    def prepare_validation_features(examples):
+        tokenized_examples = tokenizer(
+            examples[question_column_name if pad_on_right else context_column_name],
+            examples[context_column_name if pad_on_right else question_column_name],
+            truncation="only_second" if pad_on_right else "only_first",
+            max_length=max_seq_length,
+            stride=data_args.doc_stride,
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            return_token_type_ids=False, # roberta: False / bert:True
+            padding="max_length" if data_args.pad_to_max_length else False,
+        )
+
+        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+        offset_mapping = tokenized_examples["offset_mapping"]
+        tokenized_examples = set_tokenized_examples_start_end_pos(tokenized_examples, sample_mapping, offset_mapping, examples)
+        # evaluation을 위해, prediction을 context의 substring으로 변환해야합니다.
+        # corresponding example_id를 유지하고 offset mappings을 저장해야합니다.
+        tokenized_examples["example_id"] = []
+
+        for i in range(len(tokenized_examples["input_ids"])):
+            # Setting sequence id
+            sequence_ids = tokenized_examples.sequence_ids(i)
+            context_index = 1 if pad_on_right else 0
+
+            # 하나의 example이 여러개의 span을 가질 수 있음
+            sample_index = sample_mapping[i]
+            tokenized_examples["example_id"].append(examples["id"][sample_index])
+
+            # Set to None the offset_mapping을 None으로 설정해서 token position이 context의 일부인지 쉽게 판별
+            tokenized_examples["offset_mapping"][i] = [
+                (o if sequence_ids[k] == context_index else None)
+                for k, o in enumerate(tokenized_examples["offset_mapping"][i])
+            ]
+        return tokenized_examples
+
+    if training_args.do_eval:
+        eval_dataset = datasets["validation"]
+
+        # Create validation feature
+        eval_dataset = eval_dataset.map(
+            prepare_validation_features,
+            batched=True,
+            num_proc=data_args.preprocessing_num_workers,
+            remove_columns=column_names,
+            load_from_cache_file=not data_args.overwrite_cache,
+        )
+
+    # Data collator
+    # flag가 True이면 이미 max length로 padding된 상태. 아니라면 data collator에서 padding을 진행 필요
+    data_collator = DataCollatorWithPadding(
+        tokenizer, pad_to_multiple_of=8 if training_args.fp16 else None
+    )
+
+# Post-processing: start logits과 end logits을 original context의 정답과 match시킵니다.
+    def post_processing_function(examples, features, predictions, training_args):
+
+        predictions = postprocess_qa_predictions(
+            examples=examples,
+            features=features,
+            predictions=predictions,
+            max_answer_length=data_args.max_answer_length,
+            output_dir=training_args.output_dir,
+        )
+        # Metric을 구할 수 있도록 Format을 맞춰야 함
+        formatted_predictions = [
+            {"id": k, "prediction_text": v} for k, v in predictions.items()
+        ]
+        if training_args.do_predict:
+            return formatted_predictions
+
+        elif training_args.do_eval:
+            references = []
+            for ex in datasets["validation"]:
+                answers = eval(ex[answer_column_name])
+                answers["answer_start"] = [answers["answer_start"]]
+                answers["text"] = [answers["text"]]
+                references.append({"id": ex["id"], "answers": answers})
+            # references = [
+            #     {"id": ex["id"], "answers": eval(ex[answer_column_name])}
+            #     for ex in datasets["validation"]
+            # ]
+            return EvalPrediction(
+                predictions=formatted_predictions, label_ids=references
+            )
+
+    metric = load_metric("squad")
+    def compute_metrics(p: EvalPrediction):
+        return metric.compute(predictions=p.predictions, references=p.label_ids)
+
+    # Trainer init
+    trainer = QuestionAnsweringTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset if training_args.do_train else None,
+        eval_dataset=eval_dataset if training_args.do_eval else None,
+        eval_examples=datasets["validation"] if training_args.do_eval else None,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        post_process_function=post_processing_function,
+        compute_metrics=compute_metrics,
+    )
+
+    # Training
+    if training_args.do_train:
+        if last_checkpoint is not None:
+            checkpoint = last_checkpoint
+        elif os.path.isdir(model_args.model_name_or_path):
+            checkpoint = model_args.model_name_or_path
+        else:
+            checkpoint = None
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        trainer.save_model()  # Saves the tokenizer too for easy upload
+
+        metrics = train_result.metrics
+        metrics["train_samples"] = len(train_dataset)
+
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+        output_train_file = os.path.join(training_args.output_dir, "train_results.txt")
+
+        with open(output_train_file, "w") as writer:
+            logger.info("***** Train results *****")
+            for key, value in sorted(train_result.metrics.items()):
+                logger.info(f"  {key} = {value}")
+                writer.write(f"{key} = {value}\n")
+
+        # Save State
+        trainer.state.save_to_json(
+            os.path.join(training_args.output_dir, "trainer_state.json")
+        )
+
+    # Evaluation
+    if training_args.do_eval:
+        logger.info("*** Evaluate ***")
+        metrics = trainer.evaluate()
+
+        metrics["eval_samples"] = len(eval_dataset)
+
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
+
+if __name__ == "__main__":
+    main()
