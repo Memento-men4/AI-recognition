58653e9051e29f3f57349a7815cd0cc4270565fc
sehee-jeong <76215009+sehee-jeong@users.noreply.github.com>
2022-12-06 16:32:51 +0900

    Update retrieval.py

--- model/retrieval.py
+++ model/retrieval.py
@@ -31,27 +31,6 @@ class SparseRetrieval:
         context_path: Optional[str] = "../data/total_meeting_collection.json",
     ) -> NoReturn:
 
-        """
-        Arguments:
-            tokenize_fn:
-                기본 text를 tokenize해주는 함수입니다.
-                아래와 같은 함수들을 사용할 수 있습니다.
-                - lambda x: x.split(' ')
-                - Huggingface Tokenizer
-                - konlpy.tag의 Mecab
-
-            data_path:
-                데이터가 보관되어 있는 경로입니다.
-
-            context_path:
-                Passage들이 묶여있는 파일명입니다.
-
-            data_path/context_path가 존재해야합니다.
-
-        Summary:
-            Passage 파일을 불러오고 TfidfVectorizer를 선언하는 기능을 합니다.
-        """
-
         self.data_path = data_path
         with open(
             os.path.join(data_path, context_path), "r", encoding="utf-8"
@@ -76,14 +55,7 @@ class SparseRetrieval:
 
     def get_sparse_embedding(self) -> NoReturn:
 
-        """
-        Summary:
-            Passage Embedding을 만들고
-            TFIDF와 Embedding을 pickle로 저장합니다.
-            만약 미리 저장된 파일이 있으면 저장된 pickle을 불러옵니다.
-        """
-
-        # Pickle을 저장합니다.
+        # Pickle을 저장
         pickle_name = f"sparse_embedding.bin"
         tfidfv_name = f"tfidv.bin"
         emd_path = os.path.join(self.data_path, pickle_name)
@@ -107,64 +79,9 @@ class SparseRetrieval:
 
     def build_faiss(self, num_clusters=64) -> NoReturn:
 
-        """
-        Summary:
-            속성으로 저장되어 있는 Passage Embedding을
-            Faiss indexer에 fitting 시켜놓습니다.
-            이렇게 저장된 indexer는 `get_relevant_doc`에서 유사도를 계산하는데 사용됩니다.
-
-        Note:
-            Faiss는 Build하는데 시간이 오래 걸리기 때문에,
-            매번 새롭게 build하는 것은 비효율적입니다.
-            그렇기 때문에 build된 index 파일을 저정하고 다음에 사용할 때 불러옵니다.
-            다만 이 index 파일은 용량이 1.4Gb+ 이기 때문에 여러 num_clusters로 시험해보고
-            제일 적절한 것을 제외하고 모두 삭제하는 것을 권장합니다.
-        """
-
-        # indexer_name = f"faiss_clusters{num_clusters}.index"
-        # indexer_path = os.path.join(self.data_path, indexer_name)
-        # if os.path.isfile(indexer_path):
-        #     print("Load Saved Faiss Indexer.")
-        #     self.indexer = faiss.read_index(indexer_path)
-
-        # else:
-        #     p_emb = self.p_embedding.astype(np.float32).toarray()
-        #     emb_dim = p_emb.shape[-1]
-
-        #     num_clusters = num_clusters
-        #     quantizer = faiss.IndexFlatL2(emb_dim)
-
-        #     self.indexer = faiss.IndexIVFScalarQuantizer(
-        #         quantizer, quantizer.d, num_clusters, faiss.METRIC_L2
-        #     )
-        #     self.indexer.train(p_emb)
-        #     self.indexer.add(p_emb)
-        #     faiss.write_index(self.indexer, indexer_path)
-        #     print("Faiss Indexer Saved.")
-
     def retrieve(
         self, query_or_dataset: Union[str, Dataset], topk: Optional[int] = 1
     ) -> Union[Tuple[List, List], pd.DataFrame]:
-
-        """
-        Arguments:
-            query_or_dataset (Union[str, Dataset]):
-                str이나 Dataset으로 이루어진 Query를 받습니다.
-                str 형태인 하나의 query만 받으면 `get_relevant_doc`을 통해 유사도를 구합니다.
-                Dataset 형태는 query를 포함한 HF.Dataset을 받습니다.
-                이 경우 `get_relevant_doc_bulk`를 통해 유사도를 구합니다.
-            topk (Optional[int], optional): Defaults to 1.
-                상위 몇 개의 passage를 사용할 것인지 지정합니다.
-
-        Returns:
-            1개의 Query를 받는 경우  -> Tuple(List, List)
-            다수의 Query를 받는 경우 -> pd.DataFrame: [description]
-
-        Note:
-            다수의 Query를 받는 경우,
-                Ground Truth가 있는 Query (train/valid) -> 기존 Ground Truth Passage를 같이 반환합니다.
-                Ground Truth가 없는 Query (test) -> Retrieval한 Passage만 반환합니다.
-        """
 
         assert (
             self.p_embedding is not None
@@ -352,15 +269,6 @@ class SparseRetrieval:
         self, query: str, k: Optional[int] = 1
     ) -> Tuple[List, List]:
 
-        """
-        Arguments:
-            query (str):
-                하나의 Query를 받습니다.
-            k (Optional[int]): 1
-                상위 몇 개의 Passage를 반환할지 정합니다.
-        Note:
-            vocab 에 없는 이상한 단어로 query 하는 경우 assertion 발생 (예) 뙣뙇?
-        """
 
         query_vec = self.tfidfv.transform([query])
         assert (
@@ -376,16 +284,6 @@ class SparseRetrieval:
     def get_relevant_doc_bulk_faiss(
         self, queries: List, k: Optional[int] = 1
     ) -> Tuple[List, List]:
-
-        """
-        Arguments:
-            queries (List):
-                하나의 Query를 받습니다.
-            k (Optional[int]): 1
-                상위 몇 개의 Passage를 반환할지 정합니다.
-        Note:
-            vocab 에 없는 이상한 단어로 query 하는 경우 assertion 발생 (예) 뙣뙇?
-        """
 
         query_vecs = self.tfidfv.transform(queries)
         assert (
@@ -544,7 +442,7 @@ if __name__ == "__main__":
     # Elasticsearch 테스트
     retriever = ElasticRetrieval(args.index_name)
 
-    query = "대통령을 포함한 미국의 행정부 견제권을 갖는 국가 기관은?"
+    query = "세희가 오늘 3시 40분에 곱창을 먹은 장소는?"
 
     if args.use_faiss:
 
