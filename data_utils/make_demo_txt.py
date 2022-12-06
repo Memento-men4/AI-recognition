662bc50d00fdb714874c203e704e7c3a47f03003
sehee-jeong <76215009+sehee-jeong@users.noreply.github.com>
2022-12-06 16:37:59 +0900

    Update make_demo_txt.py

--- data_utils/make_demo_txt.py
+++ data_utils/make_demo_txt.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def preprocess(text):
     """
-    삽입할 데이터 전처리
+    데이터 전처리
     """
     text = re.sub(r"\n\n", " ", text)
     # text = re.sub(r"\\n", " ", text)
@@ -21,20 +21,12 @@ save_folder = "../data/demo_data/"
 df = pd.read_csv(path)
 context_df = pd.DataFrame()
 context_df["raw"] = df["context"]
-# context_df.head()
 
 
 for i in range(len(context_df)):
-    # 회의록 내용이랑 제목 분리
     data = context_df["raw"].iloc[i].split("@")
     title = preprocess(data[1])
 
-    # 회의록 띄어쓰기 보정
-    if title[-12:] == " 본 회 의 회 의 록" or title[-12:] == "본 회 의 회 의 록":
-        title = title[:-12] + " 본희의회의록"
-    content = preprocess(data[2])
-    
-    # 제목을 파일명으로 저장
     save_path = save_folder + title + ".txt"
     print(save_path)
     
